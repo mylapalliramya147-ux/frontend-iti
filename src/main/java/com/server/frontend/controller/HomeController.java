2c1465770b405525583c4e644943d3cd9edd2011
nagachaitanyaappana <nagachaitanyaappana@gmail.com>
2026-08-18 13:52:12 +0530

    removed / completely

--- src/main/java/com/server/frontend/controller/HomeController.java
+++ src/main/java/com/server/frontend/controller/HomeController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
-        return "reports/reports";
-    }
-
     @GetMapping("/authHome")
     public String authHome() {
         return "checkmeritschedule/ScheduleEntry";
