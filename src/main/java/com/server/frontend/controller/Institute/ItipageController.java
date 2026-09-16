8747151314b0f6adad2c143e5c4ae297a5ab6b0d
Appana Naga Chaitanya <nagachaitanyaappana@gmail.com>
2026-09-16 10:30:53 +0530

    resolved merge conflicts

--- src/main/java/com/server/frontend/controller/Institute/ItipageController.java
+++ src/main/java/com/server/frontend/controller/Institute/ItipageController.java
@@ -16,96 +16,34 @@ public class ItipageController {
     
     @GetMapping("/itiList")
     public String getItiList(Model model) {
-<<<<<<< HEAD
-        model.addAttribute(
-                "itiApiUrl",
-                backendApiConfig.getFullUrl("iti")
-        );
-=======
         model.addAttribute("itiApiUrl", backendApiConfig.getFullUrl("reports"));
->>>>>>> 372fda391e99b28f6728fb85f5459c1c3632db7c
         return "Institute/ItiList";
     }
 
     @GetMapping("/iti-details")
     public String itiDetails(Model model) {
-<<<<<<< HEAD
-
-        model.addAttribute(
-                "itiApiUrl",
-                backendApiConfig.getFullUrl("iti")
-        );
-
-=======
         model.addAttribute("itiApiUrl", backendApiConfig.getFullUrl("reports"));
->>>>>>> 372fda391e99b28f6728fb85f5459c1c3632db7c
         return "Institute/ItiDetails";
     }
 
     @GetMapping("/iti-create")
     public String itiCreate(Model model) {
-<<<<<<< HEAD
-
-        model.addAttribute(
-                "itiApiUrl",
-                backendApiConfig.getFullUrl("iti")
-        );
-
-        model.addAttribute(
-                "districtApiUrl",
-                backendApiConfig.getFullUrl("districts")
-        );
-
-        model.addAttribute(
-                "designationApiUrl",
-                backendApiConfig.getFullUrl("designations")
-        );
-
-        model.addAttribute(
-                "itiRegistrationApiUrl",
-                backendApiConfig.getFullUrl("iti-create")
-        );
-
-=======
         model.addAttribute("itiApiUrl", backendApiConfig.getFullUrl("reports"));
         model.addAttribute("districtApiUrl", backendApiConfig.getFullUrl("districts"));
         model.addAttribute("designationApiUrl", backendApiConfig.getFullUrl("designations"));
->>>>>>> 372fda391e99b28f6728fb85f5459c1c3632db7c
         return "Institute/ItiCreate";
     }
 
     @GetMapping("/iti-trade-selection")
     public String itiTradeSelection(Model model) {
-<<<<<<< HEAD
-        model.addAttribute(
-                "itiApiUrl",
-                backendApiConfig.getFullUrl("iti")
-        );
-        model.addAttribute(
-                "backendApiBaseUrl",
-                backendApiConfig.getBaseUrl("iti")
-        );
-        model.addAttribute(
-                "itiTradesApiUrl",
-                backendApiConfig.getFullUrl("iti-trades")
-        );
-=======
         model.addAttribute("itiApiUrl", backendApiConfig.getFullUrl("reports"));
         model.addAttribute("backendApiBaseUrl", backendApiConfig.getBaseUrl("reports"));
->>>>>>> 372fda391e99b28f6728fb85f5459c1c3632db7c
         return "Institute/ItiTradeSelection";
     }
 
     @GetMapping("/shift-unit-permitted")
     public String shiftUnitPermitted(Model model) {
-<<<<<<< HEAD
-        model.addAttribute(
-                "shiftUnitApiUrl",
-                backendApiConfig.getFullUrl("shift-unit")
-        );
-=======
         model.addAttribute("backendApiBaseUrl", backendApiConfig.getBaseUrl("reports"));
->>>>>>> 372fda391e99b28f6728fb85f5459c1c3632db7c
         return "Institute/ShiftUnitPermitted";
     }
 }
