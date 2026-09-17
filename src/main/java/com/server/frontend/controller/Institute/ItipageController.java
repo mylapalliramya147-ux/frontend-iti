8596e7863b39e22104dd9d0f64f33016b110e3db
Ramya <mylapalliramya147@gmail.com>
2026-09-17 15:06:54 +0530

    Remove redundant itiApiUrl assignments and unify API URL usage in ItipageController

--- src/main/java/com/server/frontend/controller/Institute/ItipageController.java
+++ src/main/java/com/server/frontend/controller/Institute/ItipageController.java
@@ -16,35 +16,24 @@ public class ItipageController {
     
     @GetMapping("/itiList")
     public String getItiList(Model model) {
-<<<<<<< HEAD
-        model.addAttribute(
-                "itiApiUrl",
-                backendApiConfig.getFullUrl("iti")
-        );
-=======
         model.addAttribute("itiApiUrl", backendApiConfig.getFullUrl("reports"));
->>>>>>> 372fda391e99b28f6728fb85f5459c1c3632db7c
         return "Institute/ItiList";
     }
 
     @GetMapping("/iti-details")
     public String itiDetails(Model model) {
-<<<<<<< HEAD
 
         model.addAttribute(
                 "itiApiUrl",
                 backendApiConfig.getFullUrl("iti")
         );
 
-=======
         model.addAttribute("itiApiUrl", backendApiConfig.getFullUrl("reports"));
->>>>>>> 372fda391e99b28f6728fb85f5459c1c3632db7c
         return "Institute/ItiDetails";
     }
 
     @GetMapping("/iti-create")
     public String itiCreate(Model model) {
-<<<<<<< HEAD
 
         model.addAttribute(
                 "itiApiUrl",
@@ -66,17 +55,14 @@ public class ItipageController {
                 backendApiConfig.getFullUrl("iti-create")
         );
 
-=======
         model.addAttribute("itiApiUrl", backendApiConfig.getFullUrl("reports"));
         model.addAttribute("districtApiUrl", backendApiConfig.getFullUrl("districts"));
         model.addAttribute("designationApiUrl", backendApiConfig.getFullUrl("designations"));
->>>>>>> 372fda391e99b28f6728fb85f5459c1c3632db7c
         return "Institute/ItiCreate";
     }
 
     @GetMapping("/iti-trade-selection")
     public String itiTradeSelection(Model model) {
-<<<<<<< HEAD
         model.addAttribute(
                 "itiApiUrl",
                 backendApiConfig.getFullUrl("iti")
@@ -89,23 +75,18 @@ public class ItipageController {
                 "itiTradesApiUrl",
                 backendApiConfig.getFullUrl("iti-trades")
         );
-=======
         model.addAttribute("itiApiUrl", backendApiConfig.getFullUrl("reports"));
         model.addAttribute("backendApiBaseUrl", backendApiConfig.getBaseUrl("reports"));
->>>>>>> 372fda391e99b28f6728fb85f5459c1c3632db7c
         return "Institute/ItiTradeSelection";
     }
 
     @GetMapping("/shift-unit-permitted")
     public String shiftUnitPermitted(Model model) {
-<<<<<<< HEAD
         model.addAttribute(
                 "shiftUnitApiUrl",
                 backendApiConfig.getFullUrl("shift-unit")
         );
-=======
         model.addAttribute("backendApiBaseUrl", backendApiConfig.getBaseUrl("reports"));
->>>>>>> 372fda391e99b28f6728fb85f5459c1c3632db7c
         return "Institute/ShiftUnitPermitted";
     }
 }
